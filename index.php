172a6755060b7a8b7478830cdf75fb8eabf8bc96
Aluno <aluno@devisate.local>
2023-04-06 09:03:29 -0300

    começando o projeto

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página inicial</title>
+</head>
+
+
+
+<body>
+    
+
+<nav>
+
+<div>
+    <img src="imagens/logo.png" width="45px" />
+</div>
+
+<div>
+    <h3>Fala CMF</h3>
+</div>
+
+<div>
+    <h3></h3>
+</div>
+</nav>
+
+
+
+
+</body>
+</html>
+
+
+
+<style>
+
+</style>
